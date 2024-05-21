24edf37d9c0f8d8c89a1f96a316a4289bf7c7d88
rpaulnelsonii@gmail.com <rpaulnelsonii@gmail.com>
2024-05-20 21:27:58 -0700

    Added to logic.js to switch from dark to light mode, tried to fix some bugs on the placements of thing

---
+++ assets/js/logic.js
@@ -0,0 +1,56 @@
+const themeButton = document.querySelector(".toggle")
+
+const getTheme = function() {
+    const theme = localStorage.getItem("theme") || "dark";
+
+    return theme;
+}
+
+const getBlogLocalStorage = function() {
+    const stringData = localStorage.getItem("blogs");
+
+    const data = JSON.parse(stringData);
+
+    return data || [];
+}
+
+const applyTheme = function(theme) {
+    let icon, circleColor;
+
+    if(theme === "light") {
+        icon = "🔥";
+        circleColor = "Orange";
+    } else{ 
+        icon = "🌊";
+        circleColor = "rgb(18, 72, 190)";
+    }
+
+    themeButton.textContent = icon;
+    document.body.classList = theme;
+    document.documentElement.style.setProperty("--circle-color", circleColor)
+}
+
+const saveTheme = function(theme) {
+    localStorage.setItem("theme", theme);
+}
+
+const themeSwitch = function(){
+    const theme = getTheme();
+
+    let newTheme;
+
+    if (theme === "dark"){
+        newTheme = "light";
+    } else{
+        newTheme ="dark";
+    }
+
+    applyTheme(newTheme);
+
+    saveTheme(newTheme);
+}
+
+applyTheme(getTheme());
+
+themeButton.addEventListener("click", themeSwitch);
+
