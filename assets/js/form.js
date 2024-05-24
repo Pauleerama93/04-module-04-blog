c65743506e3eb71a101c61804cc2c1d81d1bb1c1
rpaulnelsonii@gmail.com <rpaulnelsonii@gmail.com>
2024-05-23 21:32:49 -0700

    Finished my logic in form.js, created the blog html, linked the blog html with js

---
+++ assets/js/form.js
@@ -0,0 +1,48 @@
+const formElement = document.querySelector('form');
+
+const redirectPage = function() {
+    location.href = "./blog.html"
+}
+
+const storeBlogDataLocalStorage = function(data) {
+    const blogs = getBlogLocalStorage();
+    blogs.push(data);
+    // convert to string
+    const stringData = JSON.stringify(blogs);
+    //convert it back to string data
+    localStorage.setItem('blogs', stringData);
+}
+
+
+const handleFormSubmit = function(event) {
+    event.preventDefault();
+    const username = document.querySelector('#username').value.trim();
+    const title = document.querySelector('#title').value.trim();
+    const content = document.querySelector('#content').value.trim();
+
+    if(!username || !title || !content){
+        const errorElement = document.querySelector('#error');
+        errorElement.textContent = 'Please fill in all fields';
+
+        setTimeout(() => {
+            errorElement.textContent = '';
+        }, 3000);
+
+        return;
+    }
+
+    const formData = {
+        username: username,
+        title: title,
+        content: content
+    }
+
+    console.log(formData);
+
+    storeBlogDataLocalStorage(formData)
+    redirectPage();
+}
+
+
+
+formElement.addEventListener('submit', handleFormSubmit)
